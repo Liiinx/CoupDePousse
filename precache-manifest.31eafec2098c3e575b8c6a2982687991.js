ceeccf6fec7ae7acc683782d1d30757236288964
Elizaveta Patrusheva <elizaveta.np@gmail.com>
2019-01-02 15:25:24 +0100

    Updates

---
+++ precache-manifest.31eafec2098c3e575b8c6a2982687991.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "3540d81f0abc34d1a62f257e9172128a",
+    "url": "/CoupDePousse/static/media/Bonzai.3540d81f.png"
+  },
+  {
+    "revision": "c712d9e670491953e27a",
+    "url": "/CoupDePousse/static/css/main.2556b996.chunk.css"
+  },
+  {
+    "revision": "a940410a1a8214befed86baf9e229c6d",
+    "url": "/CoupDePousse/static/media/logo.a940410a.png"
+  },
+  {
+    "revision": "68b8d612d4f2f3ca289f",
+    "url": "/CoupDePousse/static/js/1.68b8d612.chunk.js"
+  },
+  {
+    "revision": "855e3f0c673f721bb534",
+    "url": "/CoupDePousse/static/js/runtime~main.855e3f0c.js"
+  },
+  {
+    "revision": "97a30ff81ac7d086f8e076955e192860",
+    "url": "/CoupDePousse/static/media/PetiteGraine.97a30ff8.png"
+  },
+  {
+    "revision": "d17925f3e2d50c0cae3125c505608e86",
+    "url": "/CoupDePousse/static/media/JeunePousse.d17925f3.png"
+  },
+  {
+    "revision": "7f2d17db9d42cddb4586fb0779cc3714",
+    "url": "/CoupDePousse/static/media/Pissenlit.7f2d17db.png"
+  },
+  {
+    "revision": "c712d9e670491953e27a",
+    "url": "/CoupDePousse/static/js/main.c712d9e6.chunk.js"
+  },
+  {
+    "revision": "e1c76cbae414a4ca52c7858ac56e105b",
+    "url": "/CoupDePousse/static/media/Cerisier.e1c76cba.png"
+  },
+  {
+    "revision": "d41cd8ca516f74d9b77cd52515ede043",
+    "url": "/CoupDePousse/static/media/Baobab.d41cd8ca.png"
+  },
+  {
+    "revision": "bf97defb93504a604fcb469b527de225",
+    "url": "/CoupDePousse/static/media/1.bf97defb.png"
+  },
+  {
+    "revision": "53de2a57e37ebc05050b13fd429daa6c",
+    "url": "/CoupDePousse/static/media/2.53de2a57.png"
+  },
+  {
+    "revision": "675784f380c491374310b239078d3974",
+    "url": "/CoupDePousse/static/media/3.675784f3.png"
+  },
+  {
+    "revision": "61f835e9737dd2c6763d18be46fe2360",
+    "url": "/CoupDePousse/static/media/4.61f835e9.png"
+  },
+  {
+    "revision": "68b8d612d4f2f3ca289f",
+    "url": "/CoupDePousse/static/css/1.2b564015.chunk.css"
+  },
+  {
+    "revision": "b53380ea71b8e76f6aec2f779c4b5a13",
+    "url": "/CoupDePousse/index.html"
+  }
+];
